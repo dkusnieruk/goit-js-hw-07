60b9aa7920654ccdba9b7038816a6ac8c10007d2
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-17 12:56:26 +0200

    update 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const getGallery = document.querySelector(`.gallery`);
+const addItem = document.createElement(`a`);
+const addImage= document.createElement(`img`);
+
+
+// getGallery.appendChild(addItem);
+// addItem.appendChild(addImage);
+
+const solutionTry = galleryItems.map((solutionOne)=>
+`<a href =${solutionOne.original}
+class="gallery__link">
+<img src =${solutionOne.preview}
+class="gallery__image"
+data-source=${solutionOne.original}
+alt=${solutionOne.description}>null</a>`);
+
+getGallery.innerHTML =solutionTry;
+
+
+
+console.log(getGallery);
