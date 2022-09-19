ab2aec812051a00ff5ad7f41e6fb8b54cde28e9a
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-19 21:44:59 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,20 +12,20 @@ const addImage= document.createElement(`img`);
 
 
 
-const pictures = galleryItems.map((picture)=>
+const pictures = galleryItems.map((picture)=> {
 `<a href =${picture.original}
 class="gallery__link"><img src =${picture.preview}
 class="gallery__image"
 data-source=${picture.original}
-alt=${picture.description}></a>`).join("");
-
+alt=${picture.description}></a>`});
+console.log(pictures);
 
 gallery.innerHTML =pictures;
 
 gallery.addEventListener(`click`, (event)=>{
 event.preventDefault();
 console.log(event.target);
-const basicPhotos = basicLightbox.create(`<img src = ${event.target}>`);
+const basicPhotos = basicLightbox.create(`<img src ${event.target}>`);
 basicPhotos.show();
 });
 
