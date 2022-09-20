39f831fb3b7f6c9a2ee3913b6b2df599aa1ac00b
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-09-20 05:00:51 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,31 +6,40 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector(`.gallery`);
-const addItem = document.createElement(`a`);
-const addImage= document.createElement(`img`);
 
 
+const solutionSearch = galleryItems.forEach((element)=>{
+    console.log(element);
+    const link=document.createElement(`a`);
+    const imageGallery=document.createElement(`img`);
+    link.setAttribute("class", "gallery__link");
+    link.setAttribute("href", `${element.original}`);
+    imageGallery.setAttribute("class", "gallery__image");
+    imageGallery.setAttribute("src", `${element.preview}`);
+    imageGallery.setAttribute("data-source", `${element.original}`);
+    imageGallery.setAttribute("alt", `${element.description}`);
 
-
-const pictures = galleryItems.map((picture)=>{ 
-const picture2=`<a href =${picture.original}
-class="gallery__link"> <img src =${picture.preview}
-class="gallery__image"
-data-source=${picture.original}
-alt=${picture.description}> </a>`
-return picture2}
-)
-
-
-
-
-
-gallery.innerHTML =pictures;
-
-gallery.addEventListener(`click`, (event)=>{
-event.preventDefault();
-console.log(event.target);
-const basicPhotos = basicLightbox.create(`<img src ${event.target}>`);
-basicPhotos.show();
+    gallery.append(link);
+    link.append(imageGallery);
 });
 
+// const pictures = galleryItems.map((picture)=>
+// `<a href =${picture.original}
+// class="gallery__link"><img src =${picture.preview}
+// class="gallery__image"
+// data-source=${picture.original}
+// alt=${picture.description}></a>`).join("");
+
+
+// gallery.innerHTML =pictures;
+
+// gallery.addEventListener(`click`, (event)=>{
+// event.preventDefault();
+// console.log(event.target);
+// const basicPhotos = basicLightbox.create(`<img ${event.target} >`);
+// basicPhotos.show();
+// });
+
+
+
+    
